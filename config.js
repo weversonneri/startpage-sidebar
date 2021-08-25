520315cee2956b393889b10f0b96f1608afb6d84
weversonneri <weversonneri@outlook.com>
2021-08-25 13:10:44 -0300

    :wrench: file for users preferences

---
+++ config.js
@@ -0,0 +1,83 @@
+const CONFIG = {
+  // General
+  name: 'Weverson',
+  twelveHourFormat: true,
+
+  // Greetings
+  greetingMorning: 'Good morning,',
+  greetingAfternoon: 'Good afternoon,',
+  greetingEvening: 'Good evening,',
+
+  // Weather
+  weatherKey: 'InsertYourAPIKeyHere123456',
+  weatherUnit: 'C',
+  weatherLatitude: '37.774929',
+  weatherLongitude: '-122.419418',
+
+  timerLocale: 'en-US',
+
+  weekday: [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ],
+
+  monthNames: [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ],
+
+  links: [
+    {
+      id: '1',
+      name: 'Github',
+      icon: 'github',
+      link: 'https://github.com/',
+    },
+    {
+      id: '2',
+      name: 'Mail',
+      icon: 'mail',
+      link: 'https://mail.protonmail.com/',
+    },
+    {
+      id: '3',
+      name: 'Todoist',
+      icon: 'trello',
+      link: 'https://calendar.google.com/calendar/r',
+    },
+    {
+      id: '4',
+      name: 'Calendar',
+      icon: 'calendar',
+      link: 'https://calendar.google.com/calendar/r',
+    },
+    {
+      id: '5',
+      name: 'Reddit',
+      icon: 'bookmark',
+      link: 'https://reddit.com',
+    },
+    {
+      id: '6',
+      name: 'Odysee',
+      icon: 'youtube',
+      link: 'https://odysee.com/',
+    },
+  ],
+
+};
